35618ed64ae60a3370bc70ece5a8a17ed5294888
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-17 16:30:16 +0900

    first commit

---
+++ disjoint_set_union/DisjointSetUnion/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DisjointSet<int> ds = new DisjointSet<int>();
+
+        string[] split = Console.ReadLine().Split(' ');
+        int n = int.Parse(split[0]);
+        int q = int.Parse(split[1]);
+        Node<int>[] nodes = new Node<int>[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            nodes[i] = ds.MakeSet(-1);
+        }
+
+        for (int i = 0; i < q; i++)
+        {
+            string[] query = Console.ReadLine().Split(' ');
+            int t = int.Parse(query[0]);
+            int u1 = int.Parse(query[1]);
+            int u2 = int.Parse(query[2]);
+
+            if (nodes[u1].Data != u1)
+                nodes[u1] = ds.MakeSet(u1);
+            if (nodes[u2].Data != u2)
+                nodes[u2] = ds.MakeSet(u2);
+
+            if (t == 0)
+            {
+                ds.UnionSet(nodes[u1], nodes[u2]);
+            }
+            else
+            {
+                bool same = ds.FindSet(nodes[u1]).Data == ds.FindSet(nodes[u2]).Data;
+                if (same)
+                {
+                    Console.WriteLine("1");
+                }
+                else
+                {
+                    Console.WriteLine("0");
+                }
+            }
+        }
+    }
+}
+
+/// <summary>
+/// node class to be used by disjoint set to represent nodes in Disjoint Set forest.
+/// </summary>
+/// <typeparam name="T">generic type for data to be stored.</typeparam>
+public class Node<T>
+{
+    public int Rank { get; set; }
+
+    public Node<T> Parent { get; set; }
+
+    public T Data { get; set; }
+
+    public Node(T data)
+    {
+        Data = data;
+        Parent = this;
+    }
+}
+
+/// <summary>
+/// Implementation of Disjoint Set with Union By Rank and Path Compression heuristics.
+/// </summary>
+/// <typeparam name="T"> generic type for implementation.</typeparam>
+public class DisjointSet<T>
+{
+    /// <summary>
+    /// make a new set and return its representative.
+    /// </summary>
+    /// <param name="x">element to add in to the DS.</param>
+    /// <returns>representative of x.</returns>
+    public Node<T> MakeSet(T x) => new Node<T>(x);
+
+    /// <summary>
+    /// find the representative of a certain node.
+    /// </summary>
+    /// <param name="node">node to find representative.</param>
+    /// <returns>representative of x.</returns>
+    public Node<T> FindSet(Node<T> node)
+    {
+        if (node != node.Parent)
+        {
+            node.Parent = FindSet(node.Parent);
+        }
+
+        return node.Parent;
+    }
+
+    /// <summary>
+    /// merge two sets.
+    /// </summary>
+    /// <param name="x">first set member.</param>
+    /// <param name="y">second set member.</param>
+    public void UnionSet(Node<T> x, Node<T> y)
+    {
+        Node<T> nx = FindSet(x);
+        Node<T> ny = FindSet(y);
+        if (nx == ny)
+        {
+            return;
+        }
+
+        if (nx.Rank > ny.Rank)
+        {
+            ny.Parent = nx;
+        }
+        else if (ny.Rank > nx.Rank)
+        {
+            nx.Parent = ny;
+        }
+        else
+        {
+            nx.Parent = ny;
+            ny.Rank++;
+        }
+    }
+}
---
+++ fenwick_tree/FenwickTree/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var sw = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+        Console.SetOut(sw);
+
+        string[] split = Console.ReadLine().Split(' ');
+        int n = int.Parse(split[0]);
+        int q = int.Parse(split[1]);
+
+        long[] array = new long[n];
+
+        string[] valueSplit = Console.ReadLine().Split(' ');
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = long.Parse(valueSplit[i]);
+        }
+        var tree = new BinaryIndexedTree(array);
+        
+
+        for (int i = 0; i < q; i++)
+        {
+            string[] vs = Console.ReadLine().Split(' ');
+            int t = int.Parse(vs[0]);
+            int x = int.Parse(vs[1]);
+            long y = long.Parse(vs[2]);
+
+            if (t == 0)
+            {
+                tree.UpdateTree(x, y);
+            }
+            else
+            {
+                Console.WriteLine($"{tree.GetSum(x, (int)y)}");
+            }
+        }
+        Console.Out.Flush();
+    }
+}
+
+
+/// <summary>
+/// Represent classical realization of Fenwiсk tree or Binary Indexed tree.
+///
+/// BITree[0..n] --> Array that represents Binary Indexed Tree.
+/// arr[0..n-1] --> Input array for which prefix sum is evaluated.
+/// </summary>
+public class BinaryIndexedTree
+{
+    private readonly long[] fenwickTree;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BinaryIndexedTree"/> class.
+    /// Create Binary indexed tree from the given array.
+    /// </summary>
+    /// <param name="array">Initial array.</param>
+    public BinaryIndexedTree(long[] array)
+    {
+        fenwickTree = new long[array.Length];
+
+        for (var i = 0; i < array.Length; i++)
+        {
+            UpdateTree(i, array[i]);
+        }
+    }
+
+    public long GetSum(int s, int e)
+    {
+        return GetSum(e) - GetSum(s);
+    }
+
+    /// <summary>
+    /// This method assumes that the array is preprocessed and
+    /// partial sums of array elements are stored in BITree[].
+    /// </summary>
+    /// <param name="index">The position to sum from.</param>
+    /// <returns>Returns sum of arr[0..index].</returns>
+    public long GetSum(int index)
+    {
+        long sum = 0;
+        var startFrom = index;
+
+        while (startFrom > 0)
+        {
+            sum += fenwickTree[startFrom - 1];
+            startFrom -= startFrom & (-startFrom);
+        }
+
+        return sum;
+    }
+
+    /// <summary>
+    /// Updates a node in Binary Index Tree at given index.
+    /// The given value 'val' is added to BITree[i] and all of its ancestors in tree.
+    /// </summary>
+    /// <param name="index">Given index.</param>
+    /// <param name="val">Value to be update on.</param>
+    public void UpdateTree(int index, long val)
+    {
+        var startFrom = index + 1;
+
+        while (startFrom <= fenwickTree.Length)
+        {
+            fenwickTree[startFrom - 1] += val;
+            startFrom += startFrom & (-startFrom);
+        }
+    }
+}
